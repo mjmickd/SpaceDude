4cc4528bc6dd8d888f9aea0d2be347cb95b341eb
mjmickd <mjmickd@gmail.com>
2023-01-20 16:56:31 -0800

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@
   /*----- state variables -----*/
 letters // line that allows you to type in letters (a-z)
 results // the word itself 
-spaceman //the actual person 
 winner // if player or computer wins string 
         //'p' if player wins, 'c' if computer wins 
 
@@ -22,7 +21,7 @@ winner // if player or computer wins string
   function init(){
 
 
-    render()
+    render();
   }
 
   function render(){
