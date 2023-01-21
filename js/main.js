ad85fb52148c4a16baa696f6c2548576509cd0d2
mjmickd <mjmickd@gmail.com>
2023-01-20 16:42:54 -0800

    Inital Commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
 
 
   /*----- state variables -----*/
+letters // line that allows you to type in letters (a-z)
+results // the word itself 
+spaceman //the actual person 
+winner // if player or computer wins string 
+        //'p' if player wins, 'c' if computer wins 
+
 
 
   /*----- cached elements  -----*/
@@ -11,3 +17,14 @@
 
 
   /*----- functions -----*/
+  init();
+  
+  function init(){
+
+
+    render()
+  }
+
+  function render(){
+
+  }
