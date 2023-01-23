912e80aad9e8c72fdc6747a4d7f7de1a35423e57
mjmickd <mjmickd@gmail.com>
2023-01-23 14:15:10 -0800

    added material

--- js/main.js
+++ js/main.js
@@ -1,35 +1,8 @@
   /*----- constants -----*/
-const LETTERS_LOOKUP = {
-A: //{image of letter}
-B: //{image of letter}
-C: //{image of letter}
-D: //{image of letter}
-E: //{image of letter}
-F: //{image of letter}
-G: //{image of letter}
-H: //{image of letter}
-I: //{image of letter}
-J: //{image of letter}
-K://{image of letter}
-L://{image of letter}
-M://{image of letter}
-N://{image of letter}
-O://{image of letter}
-P://{image of letter}
-Q://{image of letter}
-R://{image of letter}
-S://{image of letter}
-T://{image of letter}
-U://{image of letter}
-V://{image of letter}
-W://{image of letter}
-X://{image of letter}
-Y://{image of letter}
-Z://{image of letter}
-}
+
 
   /*----- state variables -----*/
-let scores; // the actual 'board'
+let letters; // an array of the letters available 
 let turn; // player 1 or -1 
 let words; //words of the game
 [Judas, Paparrazi, Just Dance, Marry the Night, You and I, The Edge of Glory, Born this Way, Rain on Me ]
@@ -45,14 +18,24 @@ const
 
 
   /*----- functions -----*/
-  init();
+  //initialize all state, then call render()
+function init(){;
   
-  function init(){
-
-
-    render();
-  }
+  letters = [
+    [Q, W, E, R, T, Y, U, I, O, P] // column 0
+    [A, S, D, F, G, H, J, K, L] // column 1
+    [Z, X, C, V, B, N, M] //column 2
+  ];
+  turn = 1; //tell you whose turn it is 
+  word = //this is going to be how you 
+      //incorporate the word that you are guessing 
+  gaga = //the images that will appear as you incorrectly guess   
+  
+  render();
+}
 
   function render(){
-
+    renderLetters(); //
+    renderWord();
+    renderGaga();
   }
