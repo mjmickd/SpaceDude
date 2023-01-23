65b066ff1c2b59b62c968ef5b4586ed1280c5a1f
mjmickd <mjmickd@gmail.com>
2023-01-23 10:48:28 -0800

    adding information

--- js/main.js
+++ js/main.js
@@ -1,16 +1,44 @@
   /*----- constants -----*/
-
+const LETTERS_LOOKUP = {
+A: //{image of letter}
+B: //{image of letter}
+C: //{image of letter}
+D: //{image of letter}
+E: //{image of letter}
+F: //{image of letter}
+G: //{image of letter}
+H: //{image of letter}
+I: //{image of letter}
+J: //{image of letter}
+K://{image of letter}
+L://{image of letter}
+M://{image of letter}
+N://{image of letter}
+O://{image of letter}
+P://{image of letter}
+Q://{image of letter}
+R://{image of letter}
+S://{image of letter}
+T://{image of letter}
+U://{image of letter}
+V://{image of letter}
+W://{image of letter}
+X://{image of letter}
+Y://{image of letter}
+Z://{image of letter}
+}
 
   /*----- state variables -----*/
-letters // line that allows you to type in letters (a-z)
-results // the word itself 
-winner // if player or computer wins string 
-        //'p' if player wins, 'c' if computer wins 
+let scores; // the actual 'board'
+let turn; // player 1 or -1 
+let winner; // 1 or -1 = winner; 
 
 
 
   /*----- cached elements  -----*/
-
+const pResultsEl = //the player results 
+const cResultsEL = // the computer results aka the word they choose?
+const 
 
   /*----- event listeners -----*/
 
