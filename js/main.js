3c01be100a3592b4ed6b1a4c3339e083cca9993f
mjmickd <mjmickd@gmail.com>
2023-01-26 11:55:58 -0800

    fixed bugs with images and answers

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const WORDS =[
   "Paparrazi"
  ];
  const IMGS =[
-  "imgs/Lady-Gaga-Meat-Suit-0.png",
+"imgs/Lady-Gaga-Meat-Suit-0.png",
  "imgs/Lady-Gaga-Meat-Suit-1.png",
  "imgs/Lady-Gaga-Meat-Suit-2.png",
  "imgs/Lady-Gaga-Meat-Suit-3.png",
@@ -34,49 +34,40 @@ const guessedEL = document.getElementById('guessed-word');
 
 
 //what stephanie said 
-const messageEl = document.getElementById('message')
-const wrongGuessesEl = document.getElementById('spotLight')
+const messageEl = document.getElementById('message');
+const wrongGuessesEl = document.getElementById('spotLight');
+const playAgainEl = document.getElementById('playButton');
 //const letterButtons=[...document.querySelectorAll('section > button')]//see Diego's code in chat 
-//const playButton= document.getElementById('playButton')//this was original reset
 const spaceDude = document.querySelector('img');
 /*----- event listeners -----*/
-document.querySelector("section").addEventListener("click", handleClick)
-
+document.querySelector("section").addEventListener("click", handleClick);
+playAgainEl.addEventListener("click", init);
 
 /*----- functions -----*/
 //initialize all state, then call render()
 init();
 
-  function init(){
-  secretWord = WORDS[Math.floor(Math.random() *WORDS.length)].toUpperCase('').split("");
-  wrongGuesses=[]
-  wordStatus = secretWord.map(ltr => ltr===" " ? " " : "_")
-  gameStatus = null;
-  console.log (secretWord, 'secret')
-  console.log(wordStatus, "Word Status")
-  render()
-  };
+function init(){
+secretWord = WORDS[Math.floor(Math.random() *WORDS.length)].toUpperCase('').split("");
+wrongGuesses=[];
+wordStatus = secretWord.map(ltr => ltr=== " " ? " " : "_");
+gameStatus = null;
+render();
+};
 
 function handleClick(evt){
- const ltr = evt.target.textContent
-  if (evt.target.tagName !== 'BUTTON' || wrongGuesses.includes(ltr)) return
-    if(secretWord.includes(ltr)){
-      secretWord.forEach(function(char, idx){
-        if(char === ltr ) wordStatus[idx] = ltr
-      })
-    } else { 
-      wrongGuesses.push(ltr)   
-    }
-    gameStatus = getGameStatus();
-  render();
+  const ltr = evt.target.textContent
+  if (evt.target.tagName !== 'BUTTON' || wrongGuesses.includes(ltr) 
+    || gameStatus) return
+  if(secretWord.includes(ltr)){
+    secretWord.forEach(function(char, idx){
+      if(char === ltr ) wordStatus[idx] = ltr
+    })
+  } else { 
+    wrongGuesses.push(ltr)   
   }
-
-
-
-function render(){
-  wrongGuessesEl.textContent = wordStatus.join(' ');
-  spaceDude.src =`imgs/Lady-Gaga-Meat-Suit-${wrongGuesses.length}.png`
-  renderMessage()
+  gameStatus = getGameStatus();
+  render();
 }
 
 function getGameStatus(){
@@ -85,6 +76,12 @@ function getGameStatus(){
   return null;
 }
 
+
+function render(){
+  wrongGuessesEl.textContent = wordStatus.join(' ');
+  spaceDude.src =`imgs/Lady-Gaga-Meat-Suit-${wrongGuesses.length}.png`;
+  renderMessage()
+}
 //render button function 
 //correct turn color, wrong turn color 
 //declare render button function 
@@ -99,12 +96,11 @@ function renderMessage(){
   if(gameStatus === "W") {
     messageEl.textContent = "YAAAS you win"
   }  else if (gameStatus === "L"){
-    messageEl.textContent = `Sorry, Try again the answer was ${secretWord.join("")}`
+    messageEl.textContent = `Sorry, Try again the answer was ${secretWord.join(" ")}`
   } else {
 messageEl.textContent = `${MAX_WRONG - wrongGuesses.length} remain, good luck`
   }
 }
-
 render() 
   
 
